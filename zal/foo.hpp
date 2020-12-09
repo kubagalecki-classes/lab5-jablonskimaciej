30ba59eafe5f9564d71da47844b835b0dbfe964e
Maciej Jabłoński <73608866+jablonskimaciej@users.noreply.github.com>
2020-12-09 21:54:14 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -4,9 +4,24 @@
 
 #include <list>
 #include <vector>
+#include <iterator>
+
+#include <iostream>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::list<Human>::iterator it;
+    int n = people.size();
+    std::vector<char> odpowiedz(people.size());
+    std::vector<char>::reverse_iterator a = odpowiedz.rbegin();
+    for (it = people.begin(); it != people.end(); it++)
+    {
+      it->birthday();
+      if (it->isMonster() == true)
+        *a = 'n';
+      else
+        *a = 'y';
+      a++;
+    }
+    return {odpowiedz};
 }
